1e42810ed1053911807c2a6cb7d1b29a97177fd4
Elena Prokhorova <eprokhorova@mail.ru>
2024-03-09 05:21:55 +0200

    add lesson2 task4

---
+++ Practice/lesson2/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 2, 4, 7, 3, 9 };
+int i = 0;
+int max = array[0];
+
+while (i < array.Length)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.Write(max);
