026f05e30c61ae09f2ccc2620c60998ee35d3fd4
Elena Prokhorova <eprokhorova@mail.ru>
2024-03-11 15:26:30 +0200

    Last homework programs languages

---
+++ homework/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿void Main ()
+{
+    int N = ReadInt ("Введите верхнюю границу чисел: ");
+    PrintNumbers(N);
+}
+
+int M = ReadInt ("Введите нижнюю границу чисел: ");
+void PrintNumbers (int number)
+{
+    if (number < M)
+    {
+        return;
+    }
+    PrintNumbers (number - 1);
+    System.Console.Write (number + " ");
+}
+int ReadInt (string msg)
+{
+    System.Console.Write(msg);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+Main ();
---
+++ homework/task3/Program.cs
@@ -0,0 +1,23 @@
+﻿void Main ()
+{
+    int N = ReadInt ("Введите верхнюю границу чисел: ");
+    PrintNumbers(N);
+}
+
+int M = ReadInt ("Введите нижнюю границу чисел: ");
+void PrintNumbers (int number)
+{
+    if (number < M)
+    {
+        return;
+    }
+    System.Console.Write (number + " ");
+    PrintNumbers (number - 1);
+    }
+
+int ReadInt (string msg)
+{
+    System.Console.Write(msg);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+Main ();
